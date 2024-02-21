e59fd5716250e1022bd14b6f90afad9c8cf7c929
Carlos Maiolino <cem@kernel.org>
2024-02-21 11:06:43 +0100

    Add test for Probe SBI Extension
    Factor out vendor id test to a new helper, and add a new test for probing the
    SBI extension.
    Compare the retrieved value against an environment variable, as the
    implementation can return any non-zero value.
    
    Signed-off-by: Carlos Maiolino <cem@kernel.org>

--- riscv/sbi.c
+++ riscv/sbi.c
@@ -14,10 +14,48 @@ static void help(void)
 	puts("An environ must be provided where expected values are given.\n");
 }
 
-int main(int argc, char **argv)
+static void check_base(void)
 {
 	struct sbiret ret;
 	long expected;
+
+	report_prefix_push("base");
+
+	if (!getenv("MVENDORID")) {
+		report_skip("mvendorid: missing MVENDORID environment variable");
+		return;
+	}
+
+	report_prefix_push("mvendorid");
+	expected = strtol(getenv("MVENDORID"), NULL, 0);
+
+	ret = sbi_ecall(SBI_EXT_BASE, SBI_EXT_BASE_GET_MVENDORID,
+			0, 0, 0, 0, 0, 0);
+
+	report(!ret.error, "no sbi.error");
+	report(ret.value == expected, "expected sbi.value");
+	report_prefix_pop();
+
+	if (!getenv("PROBE_EXT")) {
+		report_skip("probe_ext: missing PROBE_EXT environment variable");
+		return;
+	}
+
+	report_prefix_push("probe_ext");
+	expected = strtol(getenv("PROBE_EXT"), NULL, 0);
+
+	ret = sbi_ecall(SBI_EXT_BASE, SBI_EXT_BASE_PROBE_EXT,
+			SBI_EXT_BASE, 0, 0, 0, 0, 0);
+
+	report(!ret.error, "no sbi.error");
+	report(ret.value == expected, "expected sbi.value");
+	report_prefix_pop();
+
+	report_prefix_pop();
+}
+
+int main(int argc, char **argv)
+{
 
 	if (argc > 1 && !strcmp(argv[1], "-h")) {
 		help();
@@ -25,17 +63,7 @@ int main(int argc, char **argv)
 	}
 
 	report_prefix_push("sbi");
+	check_base();
 
-	if (!getenv("MVENDORID")) {
-		report_skip("mvendorid: missing MVENDORID environment variable");
-		goto done;
-	}
-	expected = strtol(getenv("MVENDORID"), NULL, 0);
-
-	ret = sbi_ecall(SBI_EXT_BASE, SBI_EXT_BASE_GET_MVENDORID, 0, 0, 0, 0, 0, 0);
-	report(!ret.error, "mvendorid: no error");
-	report(ret.value == expected, "mvendorid");
-
-done:
 	return report_summary();
 }
